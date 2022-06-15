5086522cab3d5aa6acc76ade9c29cdc0d8344de8
Torkkun <onigirikaribian@gmail.com>
2022-06-15 16:21:08 +0900

    add culmn

--- pkg/domain/merakiWebhook.go
+++ pkg/domain/merakiWebhook.go
@@ -28,5 +28,8 @@ type TriggerData struct {
 }
 
 type Trigger struct {
-	Ts int `json:"ts"`
+	Ts          int    `json:"ts"`
+	Type        string `json:"type"`
+	NodeId      int    `json:"nodeId"`
+	SensorValue int    `json:"sensorValue"`
 }
